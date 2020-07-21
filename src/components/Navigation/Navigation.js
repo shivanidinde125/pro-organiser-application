b237bfbff65d7370efe3571a0f5abb570d84b63e
rishikeshvanjare18 <59860060+rishikeshvanjare18@users.noreply.github.com>
2020-07-21 20:27:20 +0530

    first commit

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,29 @@
+import React,{Component} from 'react';
+import{
+    NavLink}
+from 'react-router-dom';
+import '../Navigation/Navigation.css';
+
+
+
+class Navigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div className="Navbar">
+                <h2 className="Navheader">Pro-Organizer</h2>
+                <NavLink className="Navlink" to="/createboard">Create a board</NavLink>
+                
+                 <NavLink className="Navlink" to="/">
+                      Home
+                 </NavLink>
+            
+             </div>
+         );
+    }
+}
+ 
+export default Navigation;
